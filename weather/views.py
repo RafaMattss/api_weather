9a2518fbe7f299b072a702ab5c2ab90043ddf8cb
RafaMattss <rafael.mattss@gmail.com>
2024-03-20 21:29:47 -0300

    update: updated views for use with serializer in mongodb

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,27 @@
+
+from datetime import datetime
+from random import randrange
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import WeatherEntity
+from .repositories import WeatherRepository
+from .serializers import WeatherSerializer
 
 class WeatherView(View):
-   
     def get(self, request):
-        return render(request, "home.html")
+        repository = WeatherRepository(collection_name='weathers')
+        weathers = repository.getAll()
+        serializer = WeatherSerializer(weathers, many=True)
+        return render(request, "home.html", {"weathers": serializer.data})
+
+class WeatherGenerate(View):
+    def get(self, request):
+        repository = WeatherRepository(collection_name='weathers')
+        weather = WeatherEntity(
+            temperature=randrange(start=17, stop=40),
+            date=datetime.now()
+        )
+        serializer = WeatherSerializer(data=weather)
+        repository.insert(serializer.data)
+
+        return redirect('Weather View')
