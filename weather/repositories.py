6be14a59a0a547231ca55c1c41223448b904c6a5
RafaMattss <rafael.mattss@gmail.com>
2024-05-23 19:27:01 -0300

    update: repositories updated

--- weather/repositories.py
+++ weather/repositories.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 import pymongo
+from weather.exceptions import WeatherException
 from pymongo.errors import ConnectionFailure
 from bson import ObjectId
-from django.conf import settings
-from .exceptions import WeatherException
 
 class WeatherRepository:
 
@@ -14,52 +14,53 @@ class WeatherRepository:
     def getConnection(self):
         try:
             client = pymongo.MongoClient(
-                getattr(settings, "MONGO_CONNECTION_STRING")
-            )
-        except ConnectionFailure as e :
-            raise WeatherException(f"Error connecting to database: {e}")
-        
+                getattr(settings, "MONGO_CONNECTION_STRING"))
+        except ConnectionFailure as e:
+            raise WeatherException(f"Error when connecting to database.(e)")
         connection = client[
             getattr(settings, "MONGO_DATABASE_NAME")]
         return connection
-    
-    def getCollection(self):
+
+    def getColletion(self):
         conn = self.getConnection()
         collection = conn[self.collection]
         return collection
     
     def getAll(self):
-        documents = []
-        for document in self.getCollection().find({}):
-            id = document.pop('_id')
-            document['id'] = str(id)
-            documents.append(document)
-        return documents
-    
-    def get(self, filter):
-        documents = []
-        for document in self.getCollection().find(filter):
-            id = document.pop('_id')
-            document['id'] = str(id)
-            documents.append(document)
-        return documents
+        document = []
+        for doc in self.getColletion().find({}):
+            id = doc.pop('_id')
+            doc['id'] = str(id)
+            print(doc)
+            document.append(doc)
+        return document
     
     def getByID(self, id):
-        document = self.getCollection().find_one({"_id": ObjectId(id)})
+        document = self.getColletion().find_one({"_id": ObjectId(id)})
         id = document.pop('_id')
         document['id'] = str(id)
         return document
     
     def insert(self, document):
-        self.getCollection().insert_one(document)
+        self.getColletion().insert_one(document)
 
     def update(self, document, id):
-        self.getCollection().update_one({"_id": ObjectId(id)}, 
-                                       {"$set": document})
-
+        self.getColletion().update_one({"_id": ObjectId(id)}, {"$set":document})
+    
     def deleteAll(self):
-        self.getCollection().delete_many({})
+        self.getColletion().delete_many({})
 
-    def deleteByID(self, id):
-        ret = self.getCollection().delete_one({"_id": ObjectId(id)})
-        return ret.deleted_count
+    def deleteById(self, id) -> None:
+       ret = self.getColletion().delete_one({"_id": ObjectId(id)})
+       return ret.deleted_count
+    
+    def getByCity(self, filter) -> None:
+        documents = []
+        for document in self.getColletion().find(filter):
+            id = document.pop('_id')
+            document['id'] = str(id)
+            documents.append(document)
+        return documents
+
+
+    
