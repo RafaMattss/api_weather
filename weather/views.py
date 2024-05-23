a67e84ed9250bc9cf06b08d22bb6b8427f609784
RafaMattss <rafael.mattss@gmail.com>
2024-05-23 19:27:25 -0300

    update: updated views file

--- weather/views.py
+++ weather/views.py
@@ -1,14 +1,39 @@
+from typing import Any
 from datetime import datetime
 from random import randrange
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views import View
 from django.shortcuts import render, redirect
+from user.authenticate import *
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import WeatherEntity
 from .repositories import WeatherRepository
 from .serializers import WeatherSerializer
 from .forms import WeatherForm
 from .exceptions import WeatherException
 
+
 class WeatherView(View):
+    
+    authenticate = False
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        #deverá vir de request
+        token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjE3MTUyOTQxMTh9.i9FjP88tzpRqGiixmC8H0LboglscfQqqXMgvq18rHkI'
+
+        error_code, _ = verifyToken(token)
+
+        if error_code == 0:
+            user = getAuthenticateUser(token)
+            self.authenticate = True
+        else:
+            self.authenticate = False
+        
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
         try:
@@ -25,11 +50,15 @@ class WeatherView(View):
                 objectReturn = {"error":serializer.errors}
         except WeatherException as e:
             objectReturn = {"error":e.message}
-  
+
+        if not self.authenticate:
+            objectReturn["errorAuth"] = "Usuário não autenticado"
+        print(objectReturn)
+
         return render(request, "home.html", objectReturn)
-    
 
 class WeatherGenerate(View):
+
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
         weather = WeatherEntity(
@@ -46,6 +75,7 @@ class WeatherGenerate(View):
         return redirect('Weather View')
     
 class WeatherReset(View):
+
     def get(self, request): 
         repository = WeatherRepository(collectionName='weathers')
         repository.deleteAll()
@@ -53,6 +83,7 @@ class WeatherReset(View):
         return redirect('Weather View')
     
 class WeatherInsert(View):
+    
     def get(self, request):
         weatherForm = WeatherForm()
 
@@ -72,14 +103,23 @@ class WeatherInsert(View):
 
         return redirect('Weather View')
     
+class WeatherDelete(View):
+
+    def get(self, request, id):
+        # Obter o repositório de clima
+        repository = WeatherRepository(collectionName='weathers')
+        # Excluir o registro de clima com o ID fornecido
+        repository.deleteById(id)
+        # Redirecionar de volta para a página principal
+        return redirect('Weather View')
 
 class WeatherEdit(View):
+
     def get(self, request, id):
         repository = WeatherRepository(collectionName='weathers')
         weather = repository.getByID(id)
         weatherForm = WeatherForm(initial=weather)
-
-        return render(request, "form_edit.html", {"form":weatherForm, "id":id})
+        return render(request, "form_edit.html", {"form":weatherForm, "id": id})
     
     def post(self, request, id):
         weatherForm = WeatherForm(request.POST)
@@ -95,26 +135,16 @@ class WeatherEdit(View):
             print(weatherForm.errors)
 
         return redirect('Weather View')
-
-
-
-
-class WeatherDelete(View):
-    def get(self, request, id):
-        repository = WeatherRepository(collectionName='weathers')
-        repository.deleteByID(id)
-
-        return redirect('Weather View')
     
-
 class WeatherFilter(View):
+
     def post(self, request):
         data = request.POST.dict()
         data.pop('csrfmiddlewaretoken')
 
         repository = WeatherRepository(collectionName='weathers')
         try:
-            weathers = list(repository.get(data))
+            weathers = list(repository.getByCity(data))
             serializer = WeatherSerializer(data=weathers, many=True)
             if (serializer.is_valid()):
                 # print('Data: ')
