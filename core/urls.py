2960dbedf87666e4364c941d653643ef635c24fb
RafaMattss <rafael.mattss@gmail.com>
2024-05-23 22:26:38 -0300

    updated: correct the code for aplication

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 #urls.py
+from pyexpat import ParserCreate
 from django.urls import path
 from weather.views import *
 from user.views import *
@@ -11,7 +12,5 @@ urlpatterns = [
     path('delete/<id>', WeatherDelete.as_view(), name="Weather Delete"),
     path('edit/<id>', WeatherEdit.as_view(), name="Weather Edit"),
     path('filter', WeatherFilter.as_view(), name="Weather Filter"),
-    path('user', UserLogin.as_view(), name="User Login"),
-    path('user/create', UserCreate.as_view(), name="User Create"),
     path('token', UserTokenizer.as_view(), name="User Token")
 ]
